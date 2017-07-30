d35ff71febc31bcf83f12b495db8772fb91f65ec
Jesse Chavez <jesse.chavez.r@gmail.com>
2017-07-30 12:26:37 +1000

    add field for file upload

--- db/migrate/20170729123239_create_documents.rb
+++ db/migrate/20170729123239_create_documents.rb
@@ -1,11 +1,10 @@
 class CreateDocuments < ActiveRecord::Migration[5.1]
   def change
     create_table :documents do |t|
-      t.references :user, foreign_key: true
+      t.references :user, foreign_key: false
       t.string :category
       t.string :name
       t.text :remarks
-      t.string :pdf_file
       t.string :url
       t.string :title
       t.text :meta_data
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170729123239) do
+ActiveRecord::Schema.define(version: 20170729235256) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,6 @@ ActiveRecord::Schema.define(version: 20170729123239) do
     t.string "category"
     t.string "name"
     t.text "remarks"
-    t.string "pdf_file"
     t.string "url"
     t.string "title"
     t.text "meta_data"
@@ -28,6 +27,7 @@ ActiveRecord::Schema.define(version: 20170729123239) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "pdf_file_data"
     t.index ["user_id"], name: "index_documents_on_user_id"
   end
 
@@ -41,5 +41,4 @@ ActiveRecord::Schema.define(version: 20170729123239) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "documents", "users"
 end
